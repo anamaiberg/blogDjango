5b9e152409c9dee89b5d59360e07f7c2ecda9573
Bhsenna <57589013+Bhsenna@users.noreply.github.com>
2022-09-15 15:55:22 -0300

    continuação blog django

--- capivarias/urls.py
+++ capivarias/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('informativo/', views.informativo, name='informativo'),
+    path('engracado/', views.engracado, name='engracado'),
+    path('fofo/', views.fofo, name='fofo'),
+    path('<int:id_capivaria>', views.conteudo, name='conteudo'),
 ]
--- capivarias/views.py
+++ capivarias/views.py
@@ -8,3 +8,27 @@ def index(requests):
     capivarias = Capivaria.objects.all()
     return render(requests, 'capivarias/index.html',
                   {'capivarias': capivarias})
+
+
+def informativo(requests):
+    capivarias = Capivaria.objects.filter(categoria="1")
+    return render(requests, 'capivarias/index.html',
+                  {'capivarias': capivarias})
+
+
+def engracado(requests):
+    capivarias = Capivaria.objects.filter(categoria="2")
+    return render(requests, 'capivarias/index.html',
+                  {'capivarias': capivarias})
+
+
+def fofo(requests):
+    capivarias = Capivaria.objects.filter(categoria="3")
+    return render(requests, 'capivarias/index.html',
+                  {'capivarias': capivarias})
+
+
+def conteudo(requests, id_capivaria):
+    capivarias = get_object_or_404(Capivaria, id=id_capivaria)
+    return render(requests, 'capivarias/conteudo.html',
+                  {'capivarias': capivarias})
